6418d2490f0921e7c5e6fc8906223c01ac499c6f
Tombra <tombra4ril@gmail.com>
2021-03-04 09:50:55 +0100

    Separated text in contact us to paragraphs

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,7 +7,9 @@ const ContactSection = () => {
       <h3>Get in touch.<br />
           Chat, write or stop by.
       </h3>
-      <p>ERIMAKIRI NIGERIA LIMITED, #6 Big Church Road, off Tombia/Amassoma Road, Yenagoa, Bayelsa State, Nigeria</p>
+      <p>ERIMAKIRI NIGERIA LIMITED </p>
+      <p>#6 Big Church Road, off Tombia/Amassoma Road </p>
+      <p>Yenagoa, Bayelsa State, Nigeria</p>
       <span>
         <a href="mailto:erimakirinigerialimited@gmail.com">
           Email Us
