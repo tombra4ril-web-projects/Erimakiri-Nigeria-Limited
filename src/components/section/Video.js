3b17c67519d4962b837b3b94783c60bf1168244d
Tombra <tombra4ril@gmail.com>
2021-07-12 21:05:07 +0100

    Added the site video and modified the video component to be responsive

--- src/components/section/Video.js
+++ src/components/section/Video.js
@@ -5,7 +5,7 @@ export const Video = () => {
   return (
     <div className="video-div sections">
       <video autoPlay controls loop>
-        <source src="/assets/The most motivational 10 seconds of video ever.mp4" type="video/mp4"/>
+        <source src="/assets/landing video.mp4" type="video/mp4"/>
         Your browser does not support the video format!!!
       </video>
     </div>
